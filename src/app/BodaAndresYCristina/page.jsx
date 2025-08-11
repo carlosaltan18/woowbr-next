70481cf386f7ca6f172083f970667c624f014d2c
carlosaltan18 <carlosaltan850@gmail.com>
2025-08-10 20:31:14 -0600

    cambios en el home

--- src/app/BodaAndresYCristina/page.jsx
+++ src/app/BodaAndresYCristina/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { InvitacionBoda } from '@/app/boda-cristina-andres/components/invitacion-agosto'
+import { InvitacionBoda } from '@/app/BodaAndresYCristina/components/invitacion-agosto'
 
 export default function Page() {
   return (
