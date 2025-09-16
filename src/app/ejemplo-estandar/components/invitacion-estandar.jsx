964aa75bb35f116b1ae5e41c79a5909c4ec0b20b
carlosaltan18 <carlosaltan850@gmail.com>
2025-09-16 17:51:40 -0600

    ejemplo de invitacion estandar

---
+++ src/app/ejemplo-estandar/components/invitacion-estandar.jsx
@@ -0,0 +1,394 @@
+'use client';
+
+import Image from 'next/image';
+import { BrandWaze, BrandWhatsapp } from 'tabler-icons-react';
+import React, { useState, useEffect, useRef } from 'react';
+import { useForm } from 'react-hook-form';
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { toast, Toaster } from "react-hot-toast";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import { Volume, VolumeOff } from 'tabler-icons-react'; 
+
+export function InvitacionBodaEstandar() {
+    const [isPlaying, setIsPlaying] = useState(true); // Estado para controlar el audio
+    const audioRef = useRef(null); // Referencia al elemento de audio
+    const [scrollProgress, setScrollProgress] = useState(0); 
+
+    const toggleAudio = () => {
+        if (isPlaying) {
+            audioRef.current.pause();
+        } else {
+            audioRef.current.play();
+        }
+        setIsPlaying(!isPlaying);
+    };
+useEffect(() => {
+        const handleScroll = () => {
+            const scrollTop = window.scrollY;
+            const windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+            const progress = (scrollTop / windowHeight) * 100;
+            setScrollProgress(progress);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+    const fotos = [
+        "https://res.cloudinary.com/dclzsvu62/image/upload/v1756603869/bodas-woowbe/vidjsw0dbcqmofzhby1i.jpg",
+        "https://res.cloudinary.com/dclzsvu62/image/upload/v1756603866/bodas-woowbe/srplewougho8u54nbsle.jpg",
+        "https://res.cloudinary.com/dclzsvu62/image/upload/v1756603912/bodas-woowbe/bxsfriphhzgytktn1uzh.jpg",
+    ];
+
+    const [fotoPrincipal, setFotoPrincipal] = useState(fotos[0]);
+
+    const sliderSettings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: false,
+        focusOnSelect: true,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
+    };
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+        reset,
+    } = useForm();
+
+    const onSubmit = async (data) => {
+        try {
+            const res = await fetch("/api/registrarInvitado/registrar", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(data),
+            });
+
+            if (!res.ok) throw new Error("Error al enviar los datos");
+
+            toast.success("Datos enviados correctamente");
+            reset(); // limpia el formulario
+        } catch (err) {
+            toast.error("Hubo un problema al enviar el formulario");
+            console.error(err);
+        }
+    };
+
+    const [tiempoRestante, setTiempoRestante] = useState({
+        dias: 0,
+        horas: 0,
+        minutos: 0,
+        segundos: 0,
+    });
+
+    useEffect(() => {
+        const fechaBoda = new Date("2025-12-05T12:00:00"); 
+        const intervalo = setInterval(() => {
+            const ahora = new Date();
+            const diferencia = fechaBoda.getTime() - ahora.getTime();
+
+            if (diferencia <= 0) {
+                clearInterval(intervalo);
+                setTiempoRestante({ dias: 0, horas: 0, minutos: 0, segundos: 0 });
+            } else {
+                const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+                const horas = Math.floor(
+                    (diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+                );
+                const minutos = Math.floor(
+                    (diferencia % (1000 * 60 * 60)) / (1000 * 60)
+                );
+                const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+
+                setTiempoRestante({ dias, horas, minutos, segundos });
+            }
+        }, 1000);
+
+        return () => clearInterval(intervalo);
+    }, []);
+    const cristinaYAndres = "https://res.cloudinary.com/dclzsvu62/image/upload/v1754591327/bodas-woowbe/mbkho0hgpj6nk5d6nhw0.png";
+    const noviosPres = "https://res.cloudinary.com/dclzsvu62/image/upload/v1754592461/bodas-woowbe/qsb6j8obe1bozozzkowl.png";
+    const logoCA = "https://res.cloudinary.com/dclzsvu62/image/upload/v1754591438/bodas-woowbe/uqpe1e73edzinn3wlx23.png";
+    const rings = "https://res.cloudinary.com/dclzsvu62/image/upload/v1754617619/bodas-woowbe/uvbyhplyhq2xgruj8rox.png";
+    const cemaco = "https://res.cloudinary.com/dclzsvu62/image/upload/v1754690226/bodas-woowbe/kowhqhfrn3oerkn5wtvh.png";
+    const regalo = "https://res.cloudinary.com/dclzsvu62/image/upload/v1754690281/bodas-woowbe/rlykjuccwkyszilrglin.png";
+    const asistencia = "https://res.cloudinary.com/dclzsvu62/image/upload/v1755609043/bodas-woowbe/gokxkcfg4plke4sezbb5.png"
+    const calendario = "https://res.cloudinary.com/dclzsvu62/image/upload/v1755611870/bodas-woowbe/al9xjqxi9kmtiridnkce.png"
+    const botella = "https://res.cloudinary.com/dclzsvu62/image/upload/v1756141616/bodas-woowbe/at9eqdskwknzrrddfwgq.png"
+    const vestimenta = "https://res.cloudinary.com/dclzsvu62/image/upload/v1756141617/bodas-woowbe/c2cpdm3z3azfwwoqlyip.png"
+    const etiqueta = "https://res.cloudinary.com/dclzsvu62/image/upload/v1756141617/bodas-woowbe/nqzfb4czbxvat2fgkmyf.png"
+    const laurel = "https://res.cloudinary.com/dclzsvu62/image/upload/v1756141616/bodas-woowbe/dopbcsk0qkrogm6w7jfo.png"
+    return (
+        <div
+            style={{ fontFamily: 'Josefin Sans' }}
+            className="font-josefin min-h-screen flex flex-col items-center justify-center px-0 relative"
+        >
+            {/* Barra de progreso */}
+            <div
+                style={{ width: `${scrollProgress}%` }}
+                className="fixed top-0 left-0 h-3 bg-[#4F6E71] z-50"
+            ></div>
+            {/* Bocina para controlar el audio */}
+            <div className="absolute top-4 right-4 z-50 cursor-pointer">
+                <Volume
+                    size={32}
+                    onClick={toggleAudio}
+                    className={`text-gray-800 hover:text-gray-600 ${
+                        isPlaying ? 'opacity-100' : 'opacity-50'
+                    }`}
+                />
+            </div>
+
+            {/* Audio de fondo */}
+            <audio ref={audioRef} autoPlay loop>
+                <source src="./BodaCristina.mp3" type="audio/mpeg" />
+                Tu navegador no soporta el elemento de audio.
+            </audio>
+
+            {/* Contenido de la invitación */}
+            <Toaster />
+            <div
+                className="w-full min-h-[40rem] h-[45rem] md:h-[50vh] lg:h-[60vh] flex flex-col justify-between text-white p-4"
+                style={{
+                    backgroundImage: `url(${cristinaYAndres})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "100% center",
+                    backgroundRepeat: "no-repeat",
+                }}
+            >
+                <div className="
+                    flex-grow flex items-start justify-start w-full
+                        md:items-center md:justify-center
+                    ">
+                    <div className="w-full flex flex-col items-start md:items-center">
+                    </div>
+                </div>
+                <Image src={logoCA} alt="Logo" width={200} height={60} className="mb-6 w-50 mx-auto" />
+            </div>
+
+
+            {/* Seccción de presentación de los novios*/}
+            <div className="w-full h-[50rem] flex flex-col items-center justify-center text-center p-8 bg-[#4F6E71] text-white">
+                <Image src={noviosPres} alt="noviosPres" width={100} height={80} className="mb-2 w-100 mx-auto" />
+            </div>
+
+
+
+            {/* Sección 3 */}
+            <div className="w-full min-h-[60rem] md:h-[70vh] lg:h-[85rem] flex flex-col items-center justify-center text-center px-4 bg-[#ffff] text-black">
+                <h2 className="text-2xl lg:text-4xl xl:text-5xl font-semibold mb-4 leading-loose">
+                    Cristina Ramírez Rodas <br /> & <br /> Andrés Sierra Búcaro
+                </h2>
+                <div className="h-[6rem] text-lg font-light max-w-md mx-auto"></div>
+                <h2 className="italic text-2xl lg:text-4xl xl:text-5xl font-light mb-15">
+                    Con la bendición de Dios y <br /> la de nuestra familia
+                </h2>
+                <h2 className="text-2xl lg:text-3xl xl:text-4xl font-light mb-4 leading-loose">
+                    <span className='font-semibold'>Padres de la novia:</span> <br /> Mynor Estuardo Ramírez Godoy <br /> Irene ValeskaRodas Figueredo
+                </h2>
+                <br />
+                <h2 className="text-2xl lg:text-3xl xl:text-4xl font-light mb-4 leading-loose">
+                    <span className='font-semibold'>Tíos del novio:</span> <br /> María Carolina Sierra Luna <br /> Roberto Sierra Luna
+                </h2>
+            </div>
+
+            {/* Sección 4 - Información */}
+            <div className="w-full py-25 px-4 text-center bg-[#1D2F23] text-white">
+                <p className="text-2xl lg:text-3xl xl:text-4xl font-thin mb-4 pb-20 ">
+                    Nos embarcamos en una nueva <br />
+                    aventura, y nuestra boda no estaría<br />
+                    completa sin la presencia de nuestros<br />
+                    seres queridos, así que queremos que<br />
+                    estes allí con nosotros.
+                </p>
+                <Image src={rings} alt="Logo" width={200} height={60} className="mb-6 w-50 mx-auto" />
+                <h2 className="pt-10 text-2xl lg:text-4xl xl:text-5xl font-light mb-4 leading-loose">
+                    <span className='font-bold'>FECHA</span>  <br /> DICIEMBRE 5, 2025
+
+                </h2>
+                <div className="w-[90%] sm:w-[90%] md:w-[90%] lg:w-[90%] xl:w-[90%] min-h-[3px] bg-white mx-auto opacity-60" />
+                <h2 className="pt-10 text-2xl lg:text-4xl xl:text-5xl font-light mb-4 leading-loose">
+                    <span className='font-bold'>Ceremonia</span>  <br /> 18:00 - 19:00 hrs. <br /> <span className=''>Capilla San Benito Abad en Cabaña Suiza</span>
+                </h2>
+                <h2 className="pt-10 text-2xl lg:text-4xl xl:text-5xl font-light mb-4 leading-loose">
+                    <span className='font-bold'>Recepción</span>  <br /> 19:30 - 00:30 hrs. <br /> <span className='font-semibold'> Plaza Helvetia Cabaña Suiza</span>
+                </h2>
+                <div className="pt-10">
+                    <a
+                        href="https://www.waze.com/es/live-map/directions/zona-1,-mixco?navigate=yes&to=place.ws.row.41014680.none"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="inline-block text-white hover:opacity-80 transition"
+                    >
+                        <BrandWaze size={48} className="mx-auto" />
+                    </a>
+                    <h4>Cabaña Suiza</h4>
+                </div>
+            </div>
+
+    
+
+            <div className="w-full py-25 px-4 text-center bg-[#627552] text-white">
+                <Image src={regalo} alt="Logo" width={50} height={40} className="mb-6 w-20 mx-auto" />
+                <h2 className="pt-10 text-2xl lg:text-4xl xl:text-5xl font-thin mb-4 ">
+                    <span className='font-bold'>MESA DE REGALOS</span> <br />  <br />  Nuestro mejor regalo es poder celebrar contigo, <br />pero si quieres
+                    tener un detalle te dejamos estas opciones:
+
+                </h2>
+                <h2 className="pt-10 text-1xl lg:text-4xl xl:text-5xl font-thin mb-4 leading-loose">
+                    <span className='text-2xl lg:text-3xl xl:text-5xl font-bold'>BANCO INDUSTRIAL</span>  <br /> <span className='text-1xl lg:text-2xl xl:text-4xl font-light'>CUENTA MONETARIA Bl-2061390247 <br /> RAMIREZ RODAS CRISTINA IRENE <br />0/ SIERRA B</span>
+                </h2>
+                <h2 className="pt-10 text-2xl lg:text-4xl xl:text-5xl font-thin mb-4 leading-loose">
+                    <span className='font-bold'>BODAS CEMACO</span></h2>
+                <div className="pt-10">
+                    <a
+                        href="https://www.bing.com/search?q=cemaco&cvid=b2c3f5e424394854a83c239ad5868d45&gs_lcrp=EgRlZGdlKgYIABBFGDkyBggAEEUYOdIBBzkxOWowajmoAgiwAgE&FORM=ANAB01&PC=ASTS"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="inline-block text-white hover:opacity-80 transition"
+                    >
+                        <Image src={cemaco} alt="Logo" width={50} height={40} className="mb-6 w-15 mx-auto" />
+                    </a>
+                </div>
+            </div>
+
+
+            {/* Sección 6 */}
+            <div className="w-full min-h-[60rem] md:h-[75rem] lg:h-[75rem] flex flex-col items-center justify-center text-center bg-[#ffff] text-black">
+                <Image src={asistencia} alt="Logo" width={100} height={60} className="mb-6 w-25 mx-auto" />
+
+                <h1 className="text-3xl lg:text-4xl xl:text-5xl font-bold mb-15">
+                    ASISTENCIA
+                </h1>
+
+                <h2 className="text-1xl lg:text-3xl xl:text-4xl font-light mb-4">
+                    Tu presencia es muy importante <br /> para nosotros, por favor confírmanos <br /> tu asistencia llenando el siguiente formulario.
+                </h2>
+                <br />
+                <h2 className="text-1xl lg:text-3xl xl:text-4xl font-light mb-4 leading-loose">
+                    <span className='font-semibold'>Hemos reservado para ti:</span> <br />
+                    <span><h1 className='text-3xl lg:text-4xl xl:text-5xl font-semibold'>1</h1></span>
+                    <span>Lugar para tí</span> <br />
+                    Fecha límite para confirmar: <br /> 2 de noviembre del 2025. <br /> Esperamos contar con tu asistencia.
+                </h2>
+
+                <div id="formulario-asistencia" className="w-full py-10 px-4 text-center bg-[#FFFF] text-black">
+                    <h2 className="text-2xl lg:text-4xl font-semibold mb-6">Confirma tu asistencia</h2>
+                    {/* Logo de WhatsApp */}
+                    <a
+                        href="https://wa.me/1234567890" // Reemplaza con el número de WhatsApp
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="inline-block mt-4"
+                    >
+                        <BrandWhatsapp
+                            size={48}
+                            className="text-green-500 hover:text-green-600 transition duration-300 mx-auto"
+                        />
+                    </a>
+                </div>
+            </div>
+            {/* Sección etinerario 
+            <div className="w-full min-h-[55rem] md:h-[70vh] lg:h-[85rem] flex flex-col items-center justify-center text-center bg-[#4c6454] text-black">
+                <Image src={ubicacion} alt="Logo" width={150} height={60} className="mb-6 w-13 mx-auto" />
+                <h1 className="text-3xl lg:text-4xl xl:text-5xl font-bold mb-15 text-white" >
+                    ETINERARIO
+                </h1>
+                <Image src={etinerario} alt="Logo" width={150} height={60} className="mb-6 mx-auto w-60 sm:w-52 md:w-60 lg:w-90 h-auto" />
+
+            </div>*/}
+            {/* Sección 7*/}
+            <div className="w-full min-h-[50rem] md:h-[70vh] lg:h-[85rem] flex flex-col items-center justify-center text-center bg-[#4c6454] text-white">
+                <Image src={calendario} alt="Logo" width={150} height={60} className="mb-6 mx-auto w-16 sm:w-16 md:w-25 lg:w-25 h-auto mt-14" />
+                <h1 className="text-2xl lg:text-4xl xl:text-5xl font-semibold mb-15 " >
+                    Calendario
+                </h1>
+                <p className="text-00.5xl lg:text-4xl xl:text-5xl font-semibold mb-15 ">
+                    No olvides guardar la fecha de <br />nuestra boda en tu calendario.
+                </p>
+                <a
+                    href="https://www.google.com/calendar/render?action=TEMPLATE&text=Boda+de+Cristina+y+Andrés&dates=20251206T000000Z/20251206T050000Z&details=¡Acompáñanos+a+celebrar+este+día+tan+especial!&location=Cabaña+Suiza,+Km20.8+Carretera+Interamericana,+Guatemala"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="mt-4 px-4 py-2 sm:px-6 sm:py-3 md:px-8 md:py-4 
+             text-sm sm:text-base md:text-lg lg:text-xl 
+             bg-[#1D2F23] text-white rounded-3xl shadow-lg 
+             hover:bg-gray-800 transition duration-300"
+                >
+                    Calendario
+                </a>
+                {/*<div className="h-[8rem] text-lg font-light max-w-md mx-auto"></div>*/}
+
+                {/* Galería de fotos */}
+                <div className="w-full py-10 px-4 text-center bg-[#4c6454]">
+                    {/* Foto principal */}
+                    <div className="mb-8">
+                        <Image
+                            src={fotoPrincipal}
+                            alt="Foto principal"
+                            width={800}
+                            height={500}
+                            className="rounded-xl shadow-lg mx-auto object-cover"
+                        />
+                    </div>
+
+                    {/* Carrusel con fotos alineadas */}
+                    <div className="w-full max-w-md sm:max-w-15xl md:max-w-4xl lg:max-w-3xl xl:max-w-2xl mx-auto">
+                        <Slider {...sliderSettings}>
+                            {fotos.map((foto, i) => (
+                                <div key={i} className="px-2">
+                                    {/* Contenedor con tamaño fijo */}
+                                    <div className="aspect-square max-w-[250px] mx-auto flex items-center justify-center bg-gray-200 rounded-lg overflow-hidden">
+                                        <Image
+                                            src={foto}
+                                            alt={`Foto ${i + 1}`}
+                                            width={500}
+                                            height={500}
+                                            className="object-cover w-full h-full cursor-pointer rounded-lg shadow-md hover:opacity-80 transition"
+                                            onClick={() => setFotoPrincipal(foto)}
+                                        />
+                                    </div>
+
+                                </div>
+                            ))}
+                        </Slider>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
---
+++ src/app/ejemplo-estandar/page.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {InvitacionBodaEstandar} from '@/app/ejemplo-estandar/components/invitacion-estandar'
+
+export default function Page() {
+  return (
+    <InvitacionBodaEstandar />
+  )
+}
