e97fbb15d29a319c534bad6ca6340f5a97425f2d
carlosaltan18 <carlosaltan850@gmail.com>
2025-09-29 22:00:04 -0600

    Fix param

--- src/app/sierraramirez2025/page.jsx
+++ src/app/sierraramirez2025/page.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { InvitacionBoda } from '@/app/sierraramirez2025/components/invitacion-agosto'
+import { Suspense } from "react";
 
 export default function Page() {
   return (
-    <InvitacionBoda />
+     <Suspense>
+      <InvitacionBoda />
+    </Suspense>
   )
 }
